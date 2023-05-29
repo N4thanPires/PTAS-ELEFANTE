13836fa5938e98ee362b2d63b0e292eaf7163569
N4thanPires <129331480+N4thanPires@users.noreply.github.com>
2023-05-29 14:01:19 -0400

    Commit Iniciar

---
+++ db.js
@@ -0,0 +1,21 @@
+const { Pool } = require('pg');
+
+let connect = async function () {
+  try {
+    if (global.connection) {
+      return Promise.resolve(global.connection);
+    }
+
+    const pool = new Pool({
+      connectionString: 'postgres://xhmccika:w432daVP2lkCmfgqpS1ZH9gktfemIepL@silly.db.elephantsql.com/xhmccika'
+    });
+
+    global.connection = pool;
+    return Promise.resolve(pool);
+  } catch (error) {
+    console.error('Erro ao estabelecer a conexão:', error);
+    throw error;
+  }
+};
+
+module.exports = { connect };
